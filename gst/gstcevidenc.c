ce2b47550ae82b06eb9d7059c33992e1c47f4e78
Michael Gruner <michael.gruner@ridgerun.com>
2013-04-03 14:40:12 -0600

    Removing comment on header since approach changed

--- gst/gstcevidenc.c
+++ gst/gstcevidenc.c
@@ -3,8 +3,6 @@
  *
  * Copyright (C) 2013 RidgeRun, LLC (http://www.ridgerun.com)
  *
- * Based on gst-libav plugin
- * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
