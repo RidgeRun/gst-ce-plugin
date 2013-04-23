05358a5a055278f17efb55265501cf7401940906
mmontero <melissa.montero@ridgerun.com>
2013-04-23 15:50:00 -0600

    Adding audio encoder initial code

---
+++ gst/gstceaudenc.c
@@ -0,0 +1,552 @@
+/*
+ * gstceaudenc.c
+ *
+ * Copyright (C) 2013 RidgeRun, LLC (http://www.ridgerun.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses>.
+ *
+ */
+
+/**
+ * SECTION:gstceaudenc
+ * @short_description: Base class for Codec Engine video encoders
+ * @see_also:
+ *
+ * This base class is for video encoders turning raw video into
+ * encoded video data using TI codecs with the AUDENC1 video encoding
+ * interface.
+ * 
+ * Subclass is responsible for providing pad template caps for
+ * source and sink pads. The pads need to be named "sink" and "src".
+ * 
+ */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <assert.h>
+#include <string.h>
+/* for stats file handling */
+#include <stdio.h>
+#include <glib/gstdio.h>
+#include <errno.h>
+
+#include <gst/gst.h>
+#include <gst/video/gstvideometa.h>
+
+#include "gstce.h"
+#include "gstceaudenc.h"
+#include <ti/sdo/ce/osal/Memory.h>
+
+enum
+{
+  PROP_0,
+
+};
+
+#define GST_CEAUDENC_GET_PRIVATE(obj)  \
+    (G_TYPE_INSTANCE_GET_PRIVATE ((obj), GST_TYPE_CEAUDENC, GstCEAudEncPrivate))
+
+struct _GstCEAudEncPrivate
+{
+  gboolean first_buffer;
+  gint32 outbuf_size;
+
+  /* Handle to the CMEM allocator */
+  GstAllocator *allocator;
+  GstAllocationParams alloc_params;
+
+  /* Codec Data */
+  Engine_Handle engine_handle;
+  XDM1_BufDesc inbuf_desc;
+  XDM_BufDesc outbuf_desc;
+};
+
+/* A number of function prototypes are given so we can refer to them later. */
+static gboolean gst_ceaudenc_open (GstAudioEncoder * encoder);
+static gboolean gst_ceaudenc_close (GstAudioEncoder * encoder);
+static gboolean gst_ceaudenc_stop (GstAudioEncoder * encoder);
+static gboolean gst_ceaudenc_set_format (GstAudioEncoder * encoder,
+    GstAudioInfo * state);
+static gboolean gst_ceaudenc_propose_allocation (GstAudioEncoder * encoder,
+    GstQuery * query);
+static GstFlowReturn gst_ceaudenc_handle_frame (GstAudioEncoder * encoder,
+    GstBuffer * buffer);
+
+static void gst_ceaudenc_set_property (GObject * object,
+    guint prop_id, const GValue * value, GParamSpec * pspec);
+static void gst_ceaudenc_get_property (GObject * object,
+    guint prop_id, GValue * value, GParamSpec * pspec);
+static void gst_ceaudenc_finalize (GObject * object);
+
+static gboolean gst_ceaudenc_reset (GstAudioEncoder * encoder);
+static gboolean gst_ceaudenc_set_dynamic_params (GstCEAudEnc * ceaudenc);
+static gboolean gst_ceaudenc_get_buffer_info (GstCEAudEnc * ceaudenc);
+
+#define gst_ceaudenc_parent_class parent_class
+G_DEFINE_TYPE (GstCEAudEnc, gst_ceaudenc, GST_TYPE_AUDIO_ENCODER);
+
+static void
+gst_ceaudenc_class_init (GstCEAudEncClass * klass)
+{
+  GObjectClass *gobject_class;
+  GstAudioEncoderClass *venc_class;
+
+  gobject_class = G_OBJECT_CLASS (klass);
+  venc_class = GST_AUDIO_ENCODER_CLASS (klass);
+
+  g_type_class_add_private (klass, sizeof (GstCEAudEncPrivate));
+
+  /* 
+   * Not using GST_DEBUG_FUNCPTR with GObject
+   * virtual functions because no one will use them
+   */
+  gobject_class->set_property = gst_ceaudenc_set_property;
+  gobject_class->get_property = gst_ceaudenc_get_property;
+  gobject_class->finalize = gst_ceaudenc_finalize;
+
+  /* 
+   * TODO:
+   * Add properties 
+   */
+
+  venc_class->open = GST_DEBUG_FUNCPTR (gst_ceaudenc_open);
+  venc_class->close = GST_DEBUG_FUNCPTR (gst_ceaudenc_close);
+  venc_class->stop = GST_DEBUG_FUNCPTR (gst_ceaudenc_stop);
+  venc_class->handle_frame = GST_DEBUG_FUNCPTR (gst_ceaudenc_handle_frame);
+  venc_class->set_format = GST_DEBUG_FUNCPTR (gst_ceaudenc_set_format);
+  venc_class->propose_allocation =
+      GST_DEBUG_FUNCPTR (gst_ceaudenc_propose_allocation);
+}
+
+static void
+gst_ceaudenc_init (GstCEAudEnc * ceaudenc)
+{
+  GstCEAudEncPrivate *priv;
+
+  GST_DEBUG_OBJECT (ceaudenc, "initialize encoder");
+
+  priv = ceaudenc->priv = GST_CEAUDENC_GET_PRIVATE (ceaudenc);
+
+  /* Allocate the codec params */
+  if (!ceaudenc->codec_params) {
+    GST_DEBUG_OBJECT (ceaudenc, "allocating codec params");
+    ceaudenc->codec_params = g_malloc0 (sizeof (AUDENC1_Params));
+    if (!ceaudenc->codec_params) {
+      GST_ELEMENT_ERROR (ceaudenc, RESOURCE, NO_SPACE_LEFT,
+          (("failed to allocate AUDENC1_Params")), (NULL));
+
+      return;
+    }
+    ceaudenc->codec_params->size = sizeof (AUDENC1_Params);
+  }
+
+  if (!ceaudenc->codec_dyn_params) {
+    GST_DEBUG_OBJECT (ceaudenc, "allocating codec dynamic params");
+    ceaudenc->codec_dyn_params = g_malloc0 (sizeof (AUDENC1_DynamicParams));
+    if (!ceaudenc->codec_dyn_params) {
+      GST_ELEMENT_ERROR (ceaudenc, RESOURCE, NO_SPACE_LEFT,
+          (("failed to allocate AUDENC1_DynamicParams")), (NULL));
+      return;
+    }
+    ceaudenc->codec_dyn_params->size = sizeof (AUDENC1_DynamicParams);
+  }
+
+  priv->first_buffer = TRUE;
+  priv->engine_handle = NULL;
+  priv->allocator = NULL;
+
+  gst_ceaudenc_reset ((GstAudioEncoder *) ceaudenc);
+}
+
+static void
+gst_ceaudenc_finalize (GObject * object)
+{
+  GstCEAudEnc *ceaudenc = GST_CEAUDENC (object);
+
+  /* Free the allocated resources */
+  if (ceaudenc->codec_params) {
+    g_free (ceaudenc->codec_params);
+    ceaudenc->codec_params = NULL;
+  }
+
+  if (ceaudenc->codec_dyn_params) {
+    g_free (ceaudenc->codec_dyn_params);
+    ceaudenc->codec_dyn_params = NULL;
+  }
+
+  G_OBJECT_CLASS (parent_class)->finalize (object);
+}
+
+/**
+ * gst_ceaudenc_configure_codec:
+ * Based on the negotiated format, create and initialize the 
+ * codec instance*/
+static gboolean
+gst_ceaudenc_configure_codec (GstCEAudEnc * ceaudenc)
+{
+  GstCEAudEncClass *klass;
+  GstCEAudEncPrivate *priv;
+  AUDENC1_Status enc_status;
+  AUDENC1_Params *params;
+  AUDENC1_DynamicParams *dyn_params;
+
+  klass = GST_CEAUDENC_CLASS (G_OBJECT_GET_CLASS (ceaudenc));
+  priv = ceaudenc->priv;
+  params = ceaudenc->codec_params;
+  dyn_params = ceaudenc->codec_dyn_params;
+
+  g_return_val_if_fail (params, FALSE);
+  g_return_val_if_fail (dyn_params, FALSE);
+  g_return_val_if_fail (klass->codec_name, FALSE);
+
+  GST_OBJECT_LOCK (ceaudenc);
+
+  if (ceaudenc->codec_handle) {
+    /* TODO: test this use case to verify its properly handled */
+    GST_DEBUG_OBJECT (ceaudenc, "Closing old codec session");
+    AUDENC1_delete (ceaudenc->codec_handle);
+  }
+
+  GST_DEBUG_OBJECT (ceaudenc, "Create the codec handle");
+  ceaudenc->codec_handle = AUDENC1_create (priv->engine_handle,
+      (Char *) klass->codec_name, params);
+  if (!ceaudenc->codec_handle)
+    goto fail_open_codec;
+
+  enc_status.size = sizeof (AUDENC1_Status);
+  enc_status.data.buf = NULL;
+
+  GST_DEBUG_OBJECT (ceaudenc, "Set codec dynamic parameters");
+  if (!gst_ceaudenc_set_dynamic_params (ceaudenc))
+    goto fail_out;
+
+  GST_OBJECT_UNLOCK (ceaudenc);
+
+  return TRUE;
+
+fail_open_codec:
+  {
+    GST_ERROR_OBJECT (ceaudenc, "failed to open codec %s", klass->codec_name);
+    GST_OBJECT_UNLOCK (ceaudenc);
+    return FALSE;
+  }
+fail_out:
+  {
+    GST_OBJECT_UNLOCK (ceaudenc);
+    AUDENC1_delete (ceaudenc->codec_handle);
+    ceaudenc->codec_handle = NULL;
+    return FALSE;
+  }
+}
+
+static gboolean
+gst_ceaudenc_set_format (GstAudioEncoder * encoder, GstAudioInfo * state)
+{
+  GstCaps *allowed_caps;
+  GstBuffer *codec_data = NULL;
+
+  GstCEAudEnc *ceaudenc = GST_CEAUDENC (encoder);
+  GstCEAudEncClass *klass = GST_CEAUDENC_CLASS (G_OBJECT_GET_CLASS (ceaudenc));
+  GstCEAudEncPrivate *priv = ceaudenc->priv;
+
+  GST_DEBUG_OBJECT (ceaudenc, "extracting common video information");
+
+  if (!gst_ceaudenc_configure_codec (ceaudenc))
+    goto fail_set_caps;
+
+  /* some codecs support more than one format, first auto-choose one */
+  GST_DEBUG_OBJECT (ceaudenc, "choosing an output format...");
+  allowed_caps = gst_pad_get_allowed_caps (GST_AUDIO_ENCODER_SRC_PAD (encoder));
+  if (!allowed_caps) {
+    GST_DEBUG_OBJECT (ceaudenc, "... but no peer, using template caps");
+    allowed_caps =
+        gst_pad_get_pad_template_caps (GST_AUDIO_ENCODER_SRC_PAD (encoder));
+  }
+  GST_DEBUG_OBJECT (ceaudenc, "chose caps %" GST_PTR_FORMAT, allowed_caps);
+
+  if (klass->set_src_caps) {
+    GST_DEBUG ("use custom set src caps");
+    if (!klass->set_src_caps (ceaudenc, &allowed_caps, &codec_data))
+      goto fail_set_caps;
+  }
+
+  /* Truncate to the first structure and fixate any unfixed fields */
+  allowed_caps = gst_caps_fixate (allowed_caps);
+
+  /*
+   * TODO
+   * Add real code for audio set format 
+   */
+  return TRUE;
+
+fail_set_caps:
+  GST_ERROR_OBJECT (ceaudenc, "couldn't set audio format");
+  return FALSE;
+}
+
+static gboolean
+gst_ceaudenc_propose_allocation (GstAudioEncoder * encoder, GstQuery * query)
+{
+  GstCEAudEnc *ceaudenc = GST_CEAUDENC (encoder);
+  GstCEAudEncPrivate *priv = ceaudenc->priv;
+  GstAllocationParams params;
+
+  params.flags = 0;
+  params.prefix = 0;
+  params.padding = 0;
+
+  /*
+   * GstAllocationParams have an alignment that is a bitmask
+   * so that align + 1 equals the amount of bytes to align to.
+   */
+  params.align = 31;
+
+  gst_query_add_allocation_param (query, priv->allocator, &params);
+
+  return GST_AUDIO_ENCODER_CLASS (parent_class)->propose_allocation (encoder,
+      query);
+}
+
+/*
+ * gst_ceaudenc_allocate_output_frame
+ * 
+ * Allocates a CMEM output buffer
+ */
+static gboolean
+gst_ceaudenc_allocate_output_frame (GstCEAudEnc * ceaudenc, GstBuffer ** buf)
+{
+  GstCEAudEncPrivate *priv = ceaudenc->priv;
+
+  /* Get allocator parameters */
+  gst_audio_encoder_get_allocator ((GstAudioEncoder *) ceaudenc, NULL,
+      &priv->alloc_params);
+
+  *buf = gst_buffer_new_allocate (priv->allocator, priv->outbuf_size,
+      &priv->alloc_params);
+
+  if (!*buf) {
+    GST_DEBUG_OBJECT (ceaudenc, "Can't alloc output buffer");
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+static GstFlowReturn
+gst_ceaudenc_handle_frame (GstAudioEncoder * encoder, GstBuffer * buffer)
+{
+
+  return GST_FLOW_OK;
+}
+
+static void
+gst_ceaudenc_set_property (GObject * object,
+    guint prop_id, const GValue * value, GParamSpec * pspec)
+{
+  GstCEAudEnc *ceaudenc;
+  GstCEAudEncClass *klass;
+  AUDENC1_Params *params;
+  AUDENC1_DynamicParams *dyn_params;
+  gboolean set_params = FALSE;
+
+  /* Get a pointer of the right type. */
+  ceaudenc = GST_CEAUDENC (object);
+  klass = GST_CEAUDENC_CLASS (G_OBJECT_GET_CLASS (ceaudenc));
+
+  if ((!ceaudenc->codec_params) || (!ceaudenc->codec_dyn_params)) {
+    GST_WARNING_OBJECT (ceaudenc,
+        "couldn't set property, no codec parameters defined");
+    return;
+  }
+
+  params = (AUDENC1_Params *) ceaudenc->codec_params;
+  dyn_params = (AUDENC1_DynamicParams *) ceaudenc->codec_dyn_params;
+
+  GST_OBJECT_LOCK (ceaudenc);
+  /* Check the argument id to see which argument we're setting. */
+  switch (prop_id) {
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+
+  if (set_params && ceaudenc->codec_handle)
+    gst_ceaudenc_set_dynamic_params (ceaudenc);
+
+  GST_OBJECT_UNLOCK (ceaudenc);
+  return;
+
+fail_static_prop:
+  GST_WARNING_OBJECT (ceaudenc, "can't set static property when "
+      "the codec is already configured");
+  GST_OBJECT_UNLOCK (ceaudenc);
+}
+
+/* The set function is simply the inverse of the get fuction. */
+static void
+gst_ceaudenc_get_property (GObject * object,
+    guint prop_id, GValue * value, GParamSpec * pspec)
+{
+  GstCEAudEnc *ceaudenc;
+  GstCEAudEncClass *klass;
+  AUDENC1_Params *params;
+  AUDENC1_DynamicParams *dyn_params;
+
+  /* It's not null if we got it, but it might not be ours */
+  ceaudenc = GST_CEAUDENC (object);
+  klass = (GstCEAudEncClass *) G_OBJECT_GET_CLASS (ceaudenc);
+
+  if ((!ceaudenc->codec_params) || (!ceaudenc->codec_dyn_params)) {
+    GST_WARNING_OBJECT (ceaudenc, "couldn't set property");
+    return;
+  }
+
+  params = (AUDENC1_Params *) ceaudenc->codec_params;
+  dyn_params = (AUDENC1_DynamicParams *) ceaudenc->codec_dyn_params;
+
+  GST_OBJECT_LOCK (ceaudenc);
+  switch (prop_id) {
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+  GST_OBJECT_UNLOCK (ceaudenc);
+}
+
+static gboolean
+gst_ceaudenc_open (GstAudioEncoder * encoder)
+{
+  GstCEAudEnc *ceaudenc = GST_CEAUDENC (encoder);
+  GstCEAudEncPrivate *priv = ceaudenc->priv;
+
+  GST_DEBUG_OBJECT (ceaudenc, "opening %s Engine", CODEC_ENGINE);
+  /* reset, load, and start DSP Engine */
+  if ((priv->engine_handle =
+          Engine_open ((Char *) CODEC_ENGINE, NULL, NULL)) == NULL)
+    goto fail_engine_open;
+
+  if (priv->allocator)
+    gst_object_unref (priv->allocator);
+
+  GST_DEBUG_OBJECT (ceaudenc, "getting CMEM allocator");
+  priv->allocator = gst_allocator_find ("ContiguousMemory");
+
+  if (!priv->allocator)
+    goto fail_no_allocator;
+
+  GST_DEBUG_OBJECT (ceaudenc, "creating slice buffer pool");
+  return TRUE;
+
+  /* Errors */
+fail_engine_open:
+  {
+    GST_ELEMENT_ERROR (ceaudenc, STREAM, CODEC_NOT_FOUND, (NULL),
+        ("failed to open codec engine \"%s\"", CODEC_ENGINE));
+    return FALSE;
+  }
+fail_no_allocator:
+  {
+    GST_WARNING_OBJECT (ceaudenc, "can't find the CMEM allocator");
+    return FALSE;
+  }
+  return TRUE;
+}
+
+static gboolean
+gst_ceaudenc_close (GstAudioEncoder * encoder)
+{
+  GstCEAudEnc *ceaudenc = GST_CEAUDENC (encoder);
+  GstCEAudEncPrivate *priv = ceaudenc->priv;
+
+  if (priv->engine_handle) {
+    GST_DEBUG_OBJECT (ceaudenc, "closing codec engine %p\n",
+        priv->engine_handle);
+    Engine_close (priv->engine_handle);
+    priv->engine_handle = NULL;
+  }
+
+  if (priv->allocator) {
+    gst_object_unref (priv->allocator);
+    priv->allocator = NULL;
+  }
+
+  return TRUE;
+}
+
+static gboolean
+gst_ceaudenc_stop (GstAudioEncoder * encoder)
+{
+  return gst_ceaudenc_reset (encoder);
+}
+
+static gboolean
+gst_ceaudenc_reset (GstAudioEncoder * encoder)
+{
+  GstCEAudEnc *ceaudenc = GST_CEAUDENC (encoder);
+  GstCEAudEncPrivate *priv = ceaudenc->priv;
+  GstCEAudEncClass *klass = (GstCEAudEncClass *) G_OBJECT_GET_CLASS (ceaudenc);
+
+  AUDENC1_Params *params = ceaudenc->codec_params;
+  AUDENC1_DynamicParams *dyn_params = ceaudenc->codec_dyn_params;
+
+  g_return_val_if_fail (params, FALSE);
+  g_return_val_if_fail (dyn_params, FALSE);
+
+  if (ceaudenc->codec_handle) {
+    AUDENC1_delete (ceaudenc->codec_handle);
+    ceaudenc->codec_handle = NULL;
+  }
+
+  GST_OBJECT_LOCK (ceaudenc);
+
+  /* Set default values for codec static params */
+
+  /* Set default values for codec dynamic params */
+
+  GST_OBJECT_UNLOCK (ceaudenc);
+
+  /* Configure specific codec */
+  if (klass->reset) {
+    GST_DEBUG_OBJECT (ceaudenc, "configuring codec");
+    klass->reset (ceaudenc);
+  }
+
+  return TRUE;
+}
+
+/* Set current dynamic parameters */
+static gboolean
+gst_ceaudenc_set_dynamic_params (GstCEAudEnc * ceaudenc)
+{
+  AUDENC1_Status enc_status;
+  gint ret;
+
+  g_return_val_if_fail (ceaudenc->codec_handle, FALSE);
+  g_return_val_if_fail (ceaudenc->codec_dyn_params, FALSE);
+
+  enc_status.size = sizeof (AUDENC1_Status);
+  enc_status.data.buf = NULL;
+
+  ret = AUDENC1_control (ceaudenc->codec_handle, XDM_SETPARAMS,
+      ceaudenc->codec_dyn_params, &enc_status);
+  if (ret != AUDENC1_EOK) {
+    GST_WARNING_OBJECT (ceaudenc, "Failed to set dynamic parameters, "
+        "status error %x, %d", (unsigned int) enc_status.extendedError, ret);
+    return FALSE;
+  }
+
+  return TRUE;
+}
---
+++ gst/gstceaudenc.h
@@ -0,0 +1,107 @@
+/*
+ * gstceaudenc.h
+ *
+ * Copyright (C) 2013 RidgeRun, LLC (http://www.ridgerun.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses>.
+ *
+ */
+
+#ifndef __GST_CE_AUDENC_H__
+#define __GST_CE_AUDENC_H__
+
+#include <gst/audio/gstaudioencoder.h>
+#include <xdc/std.h>
+#include <ti/sdo/ce/Engine.h>
+#include <ti/sdo/ce/audio1/audenc1.h>
+
+#include "gstceutils.h"
+
+G_BEGIN_DECLS typedef struct _GstCEAudEnc GstCEAudEnc;
+typedef struct _GstCEAudEncClass GstCEAudEncClass;
+typedef struct _GstCEAudEncPrivate GstCEAudEncPrivate;
+
+struct _GstCEAudEnc
+{
+  GstAudioEncoder parent;
+
+  /* Handle to the Codec */
+  AUDENC1_Handle codec_handle;
+  AUDENC1_Params *codec_params;
+  AUDENC1_DynamicParams *codec_dyn_params;
+
+  /*< private > */
+  GstCEAudEncPrivate *priv;
+
+  gpointer _gst_reserved[GST_PADDING_LARGE];
+};
+
+
+/**
+ * GstCEAudEncClass
+ * @parent_class:   Element parent class
+ * @codec_name:     The name of the codec
+ * @reset:          Optional.
+ *                  Allows subclass to set default values of properties and
+ *                  reset the element resources. Called when the element is
+ *                  initialized and when the element stops processing.
+ * @set_src_caps:   Optional.
+ *                  Allows subclass to be notified of the actual src caps
+ *                  and be able to propose custom src caps and codec data.
+ * @pre_process:    Optional.
+ *                  Called right before the base class will start the encoding 
+ *                  process. Allows delayed configuration and dynamic properties
+ *                  be performed in this method.
+ * @post_process:   Optional.
+ *                  Called after the base class finished the encoding 
+ *                  process. Allows output buffer transformations.
+ * 
+ * Subclasses can override any of the available virtual methods or not, as
+ * needed. At minimum @codec_name shoud be filled.
+ */
+struct _GstCEAudEncClass
+{
+  GstAudioEncoderClass parent_class;
+
+  /*< public > */
+  const gchar *codec_name;
+
+  /* virtual methods for subclasses */
+  void (*reset) (GstCEAudEnc * ceaudenc);
+    gboolean (*set_src_caps) (GstCEAudEnc * ceaudenc,
+      GstCaps ** src_caps, GstBuffer ** codec_data);
+
+    gboolean (*pre_process) (GstCEAudEnc * ceaudenc, GstBuffer * input_buffer);
+    gboolean (*post_process) (GstCEAudEnc * ceaudenc,
+      GstBuffer * output_buffer);
+
+  /*< private > */
+  gpointer _gst_reserved[GST_PADDING_LARGE];
+};
+
+#define GST_TYPE_CEAUDENC \
+  (gst_ceaudenc_get_type())
+#define GST_CEAUDENC(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_CEAUDENC,GstCEAudEnc))
+#define GST_CEAUDENC_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_CEAUDENC,GstCEAudEncClass))
+#define GST_IS_CEAUDENC(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_CEAUDENC))
+#define GST_IS_CEAUDENC_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_CEAUDENC))
+
+GType gst_ceaudenc_get_type (void);
+
+G_END_DECLS
+#endif /* __GST_CE_AUDENC_H__ */
